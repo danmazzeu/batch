51d2c2e0321404b614de7fee92a002d0d2771516
Daniel Mazzeu <danmazzeu9@gmail.com>
2024-12-23 10:55:11 -0300

    Update

--- setup_github.sh
+++ setup_github.sh
@@ -2,20 +2,34 @@
 
 # Function to check if Git is installed
 function check_git() {
-    if ! command -v git &> /dev/null; then
-        echo "Git is not installed. Would you like to install it now? (y/n)"
-        read -r install_git
-        if [[ $install_git =~ ^[Yy]$ ]]; then
-            sudo apt-get update && sudo apt-get install git -y
-            check_git
-        else
-            echo "Git installation is recommended for this script to function."
-            exit 1
-        fi
+  if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Would you like to install it now? (y/n)"
+    read -r install_git
+    if [[ $install_git =~ ^[Yy]$ ]]; then
+      sudo apt-get update && sudo apt-get install git -y
+      check_git
+    else
+      echo "Git installation is recommended for this script to function."
+      exit 1
     fi
+  fi
 }
 
 # Checks if Git is installed
 check_git
 
-echo "https://<danmazzeu>:<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>@github.com" >> ~/.git-credentials
+read -p "Digite seu token: " -s token
+
+read -p "Digite seu usuário: "  user
+
+read -p "Digite seu repositorio: "  repo
+
+
+# Construct secure Git remote URL
+git_remote_url="https://${token}@github.com/${user}/${repo}"
+
+git push --set-upstream origin main
+git remote add origin "$git_remote_url"
+git push origin
+
+echo "Successfully pushed to remote repository."
